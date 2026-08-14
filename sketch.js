d18edc8d2c4dc32810d31f7a77f2344309904e3f
sorabora <doompi.webs@gmail.com>
2026-08-14 21:15:59 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5792,7 +5792,7 @@ function draw() {
   runHook("draw:main", { rocket: curRocket, camera });
   textSize(12);
   fill("white");
-  text("v1.3.0 [Public Alpha]", width - 120, height - 40);
+  text("v1.4.0 [Public Beta]", width - 120, height - 40);
 
   if (careerMode) {
     drawCostBox();
